03c2b1168da727da90fdb63025681d20f932328d
nbsteuv <nbsteuv@gmail.com>
2016-11-29 07:03:34 -0500

    Changed Main test to EulerApp test

---
+++ app/tests/components/EulerApp.test.jsx
@@ -0,0 +1,9 @@
+var expect = require('expect');
+
+var EulerApp = require('EulerApp');
+
+describe('EulerApp', () => {
+  it('should exist', () => {
+    expect(EulerApp).toExist();
+  });
+});
--- app/tests/components/Main.test.jsx
+++
@@ -1,9 +0,0 @@
-var expect = require('expect');
-
-var Main = require('Main');
-
-describe('Main', () => {
-  it('should exist', () => {
-    expect(Main).toExist();
-  });
-});
