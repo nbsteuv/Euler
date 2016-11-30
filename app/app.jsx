6a7b6acce7e37d1dcb17934c963f0f888987d35f
nbsteuv <nbsteuv@gmail.com>
2016-11-29 20:44:21 -0500

    Added store and sample data

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {Provider} = require('react-redux');
+
+var store = require('configureStore').configure();
+
+var state = store.getState();
+console.log(state);
 
 var EulerApp = require('EulerApp');
 var Exercise = require('Exercise');
@@ -12,11 +18,13 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
+  <Provider store={store}>
     <Router history={hashHistory}>
       <Route path="/" component={EulerApp}>
         <IndexRoute component={Exercise} />
         <Route path="/test" component={Test} />
       </Route>
-    </Router>,
+    </Router>
+  </Provider>,
   document.getElementById('app')
 );
---
+++ app/store/configureStore.jsx
@@ -0,0 +1,61 @@
+var redux = require('redux');
+
+export var configure = (initialState = {
+  problems: [
+  	{
+  		id: 1,
+  		imageFile: 'path/to/problem/1',
+  		currentCode: 'Current test code',
+  		codeHistory: ['Current code', 'Old code'],
+  		languages: ['php', 'javascript', 'csharp']
+  	},
+  	{
+  		id: 2,
+  		imageFile: 'path/to/problem/2',
+  		currentCode: 'Current test code',
+  		codeHistory: ['Current code', 'Old code'],
+  		languages: ['php', 'javascript', 'csharp']
+  	},
+  	{
+  		id: 3,
+  		imageFile: 'path/to/problem/3',
+  		currentCode: 'Current test code',
+  		codeHistory: ['Current code', 'Old code'],
+  		languages: ['php', 'javascript', 'csharp']
+  	},
+  	{
+  		id: 4,
+  		imageFile: 'path/to/problem/4',
+  		currentCode: 'Current test code',
+  		codeHistory: ['Current code', 'Old code'],
+  		languages: ['php', 'javascript', 'csharp']
+  	},
+  ],
+
+  languages: [
+  	{
+  		name: 'php',
+  		displayName: 'PHP',
+  		imageFile: 'path/to/languageLogo/php'
+  	},
+  	{
+  		name: 'javascript',
+  		displayName: 'JavaScript',
+  		imageFile: 'path/to/languageLogo/javascript'
+  	},
+  	{
+  		name: 'csharp',
+  		displayName: 'C#',
+  		imageFile: 'path/to/languageLogo/csharp'
+  	}
+  ]
+}) => {
+
+  var reducer = (state = initialState, action) => {
+    return state;
+  }
+
+  var store = redux.createStore(reducer);
+
+  return store;
+};
