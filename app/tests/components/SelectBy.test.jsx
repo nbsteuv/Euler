07137bf047da91c6694730150616889ee822dddd
nbsteuv <nbsteuv@gmail.com>
2016-12-03 14:17:01 -0500

    Added option list render test for SelectBy component

--- app/tests/components/SelectBy.test.jsx
+++ app/tests/components/SelectBy.test.jsx
@@ -1,9 +1,22 @@
 var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
 
-var SelectBy = require('SelectBy');
+import ConnectedSelectBy,{SelectBy} from 'SelectBy';
 
 describe('SelectBy', () => {
   it('should exist', () => {
     expect(SelectBy).toExist();
+  });
+
+  it('should render each selectByOption', () => {
+    var selectByOptions = ['One', 'Two', 'Three'];
+    var selectByProp = 'One';
+    var selectBy = TestUtils.renderIntoDocument(<SelectBy selectByOptions={selectByOptions} selectBy={selectByProp} />);
+    var $el = $(ReactDOM.findDOMNode(selectBy));
+    var numberOptions = $el.find('input').length;
+    expect(numberOptions).toBe(3);
   });
 });
