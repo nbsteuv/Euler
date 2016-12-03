09acb07a1790d5fae3e5ecc565c07d7e7cbd45cc
nbsteuv <nbsteuv@gmail.com>
2016-12-03 13:32:42 -0500

    Added SelectBy render test for Exercise component

--- app/tests/components/EulerApp.test.jsx
+++ app/tests/components/EulerApp.test.jsx
@@ -2,6 +2,8 @@ var expect = require('expect');
 
 var EulerApp = require('EulerApp');
 
+//TODO: Test that Exercise or Test components rendered on correct routing
+
 describe('EulerApp', () => {
   it('should exist', () => {
     expect(EulerApp).toExist();
--- app/tests/components/Exercise.test.jsx
+++ app/tests/components/Exercise.test.jsx
@@ -1,9 +1,29 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 var expect = require('expect');
+var {Provider} = require('react-redux');
+var TestUtils = require('react-addons-test-utils');
 
-var Exercise = require('Exercise');
+import Exercise from 'Exercise';
+import SelectBy from 'SelectBy';
+import {configure} from 'configureStore';
 
 describe('Exercise', () => {
   it('should exist', () => {
     expect(Exercise).toExist();
+  });
+
+  describe('rendering', () => {
+    it('should render SelectBy component', () => {
+      //Leaving configure with no arguments gives default initial state
+      var store = configure();
+      var exercise = TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          <Exercise />
+        </Provider>
+      );
+      var selectBy = TestUtils.scryRenderedComponentsWithType(exercise, SelectBy);
+      expect(selectBy.length).toBe(1);
+    });
   });
 });
