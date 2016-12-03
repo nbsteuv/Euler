a329dd46803650d7986045e7ea96f74064f504de
nbsteuv <nbsteuv@gmail.com>
2016-12-03 15:03:03 -0500

    Added render language list test for LanguageSelector component

--- app/tests/components/LanguageSelector.test.jsx
+++ app/tests/components/LanguageSelector.test.jsx
@@ -1,9 +1,32 @@
 var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
 
-var LanguageSelector = require('LanguageSelector');
+import {LanguageSelector} from 'LanguageSelector';
 
 describe('LanguageSelector', () => {
   it('should exist', () => {
     expect(LanguageSelector).toExist();
+  });
+
+  it('should render language images from language list', () => {
+    var languages = [
+      {
+        name: 'langugage1',
+        imageFile: 'images/language1.jpg',
+        displayName: 'Language 1'
+      },
+      {
+        name: 'langugage2',
+        imageFile: 'images/language2.jpg',
+        displayName: 'Language 2'
+      }
+    ];
+    var languageSelector = TestUtils.renderIntoDocument(<LanguageSelector languageList={languages} />);
+    var $el = $(ReactDOM.findDOMNode(languageSelector));
+    var numberLanguages = $el.find('span').length;
+    expect(numberLanguages).toBe(2);
   });
 });
