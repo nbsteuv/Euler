3a90f19f3c6cf0c5adb722ee99a0088d9d0a20d7
nbsteuv <nbsteuv@gmail.com>
2016-12-03 15:13:39 -0500

    Added active class language test for LanguageSelector component

--- app/tests/components/LanguageSelector.test.jsx
+++ app/tests/components/LanguageSelector.test.jsx
@@ -29,4 +29,25 @@ describe('LanguageSelector', () => {
     var numberLanguages = $el.find('span').length;
     expect(numberLanguages).toBe(2);
   });
+
+  it('should add active className to selected language in list', () => {
+    var languages = [
+      {
+        name: 'language1',
+        imageFile: 'images/language1.jpg',
+        displayName: 'Language 1'
+      },
+      {
+        name: 'language2',
+        imageFile: 'images/language2.jpg',
+        displayName: 'Language 2'
+      }
+    ];
+    var selectedLanguage = 'language1';
+    var languageSelector = TestUtils.renderIntoDocument(<LanguageSelector languageList={languages} selectedLanguage={selectedLanguage} />);
+    var $el = $(ReactDOM.findDOMNode(languageSelector));
+    var firstSpan = $el.find('span')[0];
+    var firstClass = $(firstSpan).attr('class');
+    expect(firstClass).toBe('language-logo language-active');
+  })
 });
