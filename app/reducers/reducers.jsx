cab1262a52b711a2c10c31353c53bd7159b3d0a1
nbsteuv <nbsteuv@gmail.com>
2016-12-03 15:41:08 -0500

    Added reducers, combined them in store config, and moved test data

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,72 @@
+export var problemsReducer = (state = [
+  {
+    id: 1,
+    imageFile: 'path/to/problem/1',
+    currentCode: 'Current test code',
+    codeHistory: ['Current code', 'Old code'],
+    languages: ['php', 'javascript', 'csharp']
+  },
+  {
+    id: 2,
+    imageFile: 'path/to/problem/2',
+    currentCode: 'Current test code',
+    codeHistory: ['Current code', 'Old code'],
+    languages: ['php', 'javascript', 'csharp']
+  },
+  {
+    id: 3,
+    imageFile: 'path/to/problem/3',
+    currentCode: 'Current test code',
+    codeHistory: ['Current code', 'Old code'],
+    languages: ['php', 'javascript', 'csharp']
+  },
+  {
+    id: 4,
+    imageFile: 'path/to/problem/4',
+    currentCode: 'Current test code',
+    codeHistory: ['Current code', 'Old code'],
+    languages: ['php', 'javascript', 'csharp']
+  },
+], action) => {
+  switch(action.type){
+    default:
+      return state;
+  }
+}
+
+export var languagesReducer = (state = [
+  {
+    name: 'php',
+    displayName: 'PHP',
+    imageFile: '/images/languages/php.jpg'
+  },
+  {
+    name: 'javascript',
+    displayName: 'JavaScript',
+    imageFile: '/images/languages/js.jpg'
+  },
+  {
+    name: 'csharp',
+    displayName: 'C#',
+    imageFile: '/images/languages/csharp.jpg'
+  }
+], action) => {
+  switch(action.type){
+    default:
+      return state;
+  }
+}
+
+export var selectByReducer = (state = 'Problem', action) => {
+  switch(action.type){
+    default:
+      return state;
+  }
+}
+
+export var selectByOptionsReducer = (state = ['Problem', 'Language'], action) => {
+  switch(action.type){
+    default:
+      return state;
+  }
+}
--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -1,62 +1,14 @@
 var redux = require('redux');
+var {problemsReducer, languagesReducer, selectByReducer, selectedProblemReducer, selectedLanguageReducer, selectByOptionsReducer} = require('reducers');
 
-export var configure = (initialState = {
-  problems: [
-  	{
-  		id: 1,
-  		imageFile: 'path/to/problem/1',
-  		currentCode: 'Current test code',
-  		codeHistory: ['Current code', 'Old code'],
-  		languages: ['php', 'javascript', 'csharp']
-  	},
-  	{
-  		id: 2,
-  		imageFile: 'path/to/problem/2',
-  		currentCode: 'Current test code',
-  		codeHistory: ['Current code', 'Old code'],
-  		languages: ['php', 'javascript', 'csharp']
-  	},
-  	{
-  		id: 3,
-  		imageFile: 'path/to/problem/3',
-  		currentCode: 'Current test code',
-  		codeHistory: ['Current code', 'Old code'],
-  		languages: ['php', 'javascript', 'csharp']
-  	},
-  	{
-  		id: 4,
-  		imageFile: 'path/to/problem/4',
-  		currentCode: 'Current test code',
-  		codeHistory: ['Current code', 'Old code'],
-  		languages: ['php', 'javascript', 'csharp']
-  	},
-  ],
-  languages: [
-  	{
-  		name: 'php',
-  		displayName: 'PHP',
-  		imageFile: '/images/languages/php.jpg'
-  	},
-  	{
-  		name: 'javascript',
-  		displayName: 'JavaScript',
-  		imageFile: '/images/languages/js.jpg'
-  	},
-  	{
-  		name: 'csharp',
-  		displayName: 'C#',
-  		imageFile: '/images/languages/csharp.jpg'
-  	}
-  ],
-  selectBy: 'Problem',
-  selectedProblem: undefined,
-	selectedLanguage: undefined,
-	selectByOptions: ['Problem', 'Language'],
-}) => {
+export var configure = () => {
 
-  var reducer = (state = initialState, action) => {
-    return state;
-  }
+  var reducer = redux.combineReducers({
+    problems: problemsReducer,
+    languages: languagesReducer,
+    selectBy: selectByReducer,
+    selectByOptions: selectByOptionsReducer
+  })
 
   var store = redux.createStore(reducer);
 
