516877b1ed35bb1d1d6609b8a800b2e2bd2d62c3
nbsteuv <nbsteuv@gmail.com>
2016-12-03 14:43:37 -0500

    Added render problem list dropdown test for ProblemSelector component

--- app/tests/components/ProblemSelector.test.jsx
+++ app/tests/components/ProblemSelector.test.jsx
@@ -1,9 +1,31 @@
 var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
 
-var ProblemSelector = require('ProblemSelector');
+import {ProblemSelector} from 'ProblemSelector';
 
 describe('ProblemSelector', () =>{
   it('should exist', () => {
     expect(ProblemSelector).toExist();
+  });
+
+  it('should render problems from problem list into select', () => {
+    var problems = [
+      {
+        id: 1
+      },
+      {
+        id: 2
+      },
+      {
+        id:3
+      }
+    ];
+    var problemSelector = TestUtils.renderIntoDocument(<ProblemSelector problemList={problems} />);
+    var $el = $(ReactDOM.findDOMNode(problemSelector));
+    var numberProblems = $el.find('option').length;
+    expect(numberProblems).toBe(3);
   });
 });
