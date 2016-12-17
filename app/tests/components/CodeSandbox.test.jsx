a722f21adfbc22b401b3ca698eb731e2286bfc43
nbsteuv <nbsteuv@gmail.com>
2016-12-17 12:52:19 -0500

    Added AceEditor render test for CodeSandbox

--- app/tests/components/CodeSandbox.test.jsx
+++ app/tests/components/CodeSandbox.test.jsx
@@ -1,9 +1,22 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
 
 var CodeSandbox = require('CodeSandbox');
+import AceEditor from 'react-ace';
 
 describe('CodeSandbox', () => {
   it('should exist', () => {
     expect(CodeSandbox).toExist();
+  });
+
+  describe('Render', () => {
+    it('should render Ace Editor', () => {
+      var codeSandbox = TestUtils.renderIntoDocument(<CodeSandbox />);
+      var aceEditors = TestUtils.scryRenderedComponentsWithType(codeSandbox, AceEditor);
+      expect(aceEditors.length).toBe(1);
+    });
   });
 });
