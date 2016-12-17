5dad4615f1fce705aaf200c1c1560eb04fcaf3f0
nbsteuv <nbsteuv@gmail.com>
2016-12-17 12:14:17 -0500

    Added CodeResult with answer and seconds render test'

--- app/tests/components/CodeResult.test.jsx
+++ app/tests/components/CodeResult.test.jsx
@@ -1,9 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
 
 var CodeResult = require('CodeResult');
 
 describe('CodeResult', () => {
   it('should exist', () => {
     expect(CodeResult).toExist();
+  });
+
+  it('should render component when answer and second props are provided', () => {
+    var codeResult = TestUtils.renderIntoDocument(<CodeResult answer={45} seconds={23} />);
+    var $el = $(ReactDOM.findDOMNode(codeResult));
+    expect($el.length).toBe(1);
   });
 });
