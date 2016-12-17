3c622c80b056cee78fed59dfc85d03ca4a24a790
nbsteuv <nbsteuv@gmail.com>
2016-12-17 12:44:41 -0500

    Added existence test for CodeSandbox

---
+++ app/tests/components/CodeSandbox.test.jsx
@@ -0,0 +1,9 @@
+var expect = require('expect');
+
+var CodeSandbox = require('CodeSandbox');
+
+describe('CodeSandbox', () => {
+  it('should exist', () => {
+    expect(CodeSandbox).toExist();
+  });
+});
