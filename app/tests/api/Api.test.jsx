c59b4844876812288e2792b6aa34fd0fffc588fe
nbsteuv <nbsteuv@gmail.com>
2016-12-16 23:26:01 -0500

    Added filter language by given problem test for API

--- app/tests/api/Api.test.jsx
+++ app/tests/api/Api.test.jsx
@@ -18,4 +18,22 @@ describe('API', () => {
     var res = API.getProblemDetails(selectedProblemId, problems);
     expect(res).toEqual(problems[1]);
   });
+
+  it('should filter language list by given problem', () => {
+    var problem = {
+      id: 5,
+      languages: ['testLanguage1']
+    }
+    var languages = [
+      {
+        name: 'testLanguage1',
+      },
+      {
+        name: 'testLanguage2',
+      }
+    ];
+    var res = API.filterLanguagesByProblem(problem, languages);
+    expect(res).toEqual([languages[0]]);
+  });
+
 });
