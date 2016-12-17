67bb04fd7b9e86d688328d84cf45d111765e3c08
nbsteuv <nbsteuv@gmail.com>
2016-12-17 13:03:41 -0500

    Added existence test for CodeWindow

---
+++ app/tests/components/CodeWindow.test.jsx
@@ -0,0 +1,9 @@
+var expect = require('expect');
+
+var CodeWindow = require('CodeWindow');
+
+describe('CodeWindow', () => {
+  it('should exist', () => {
+    expect(CodeWindow).toExist();
+  });
+});
