3430e4bc41b979012656f61ee11988cd42c34c52
nbsteuv <nbsteuv@gmail.com>
2016-12-17 11:08:47 -0500

    Added CodeError existence test

---
+++ app/tests/components/CodeError.test.jsx
@@ -0,0 +1,11 @@
+var expect = require('expect');
+
+var CodeError = require('CodeError');
+
+describe('Code Error', () => {
+  it('should exist', () => {
+    expect(CodeError).toExist();
+  });
+
+  
+})
