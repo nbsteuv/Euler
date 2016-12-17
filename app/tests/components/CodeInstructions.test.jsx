990e5217f2aff2bafbe5458c33cbf55aed7b9928
nbsteuv <nbsteuv@gmail.com>
2016-12-17 11:48:34 -0500

    Added hardcoded instruction render test

--- app/tests/components/CodeInstructions.test.jsx
+++ app/tests/components/CodeInstructions.test.jsx
@@ -10,4 +10,12 @@ describe('CodeInstructions', () => {
   it('should exist', () => {
     expect(CodeInstructions).toExist();
   });
+
+  it('should render hardcoded instructions', () => {
+    var codeInstructions = TestUtils.renderIntoDocument(<CodeInstructions />);
+    var hardCodedInstructions = "Enter your code below. When run as a function, your code should return the answer.";
+    var $el = $(ReactDOM.findDOMNode(codeInstructions));
+    var renderedInstructions = $el.find('.code-instructions').text();
+    expect(renderedInstructions).toBe(hardCodedInstructions);
+  });
 });
