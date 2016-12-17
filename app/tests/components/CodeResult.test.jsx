3ec1a05cdb38c22f24874612e33ca0cfcfef0eec
nbsteuv <nbsteuv@gmail.com>
2016-12-17 11:57:32 -0500

    Added existence test for CodeResult'

---
+++ app/tests/components/CodeResult.test.jsx
@@ -0,0 +1,9 @@
+var expect = require('expect');
+
+var CodeResult = require('CodeResult');
+
+describe('CodeResult', () => {
+  it('should exist', () => {
+    expect(CodeResult).toExist();
+  });
+});
