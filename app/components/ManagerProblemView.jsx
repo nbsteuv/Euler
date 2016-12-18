71d0a99034f9905b00e805bc4155aac4ea902ba1
nbsteuv <nbsteuv@gmail.com>
2016-12-18 15:49:27 -0500

    Added renderFocusView method for conditionally rendering problem editor

--- app/components/ManagerProblemView.jsx
+++ app/components/ManagerProblemView.jsx
@@ -3,10 +3,16 @@ var {connect} = require('react-redux');
 
 var ManagerProblemView = React.createClass({
   render: function(){
-    var {problem} = this.props;
+    var {problem, selectedProblemView} = this.props;
+    var renderFocusView = () => {
+      if(selectedProblemView == problem.id){
+        return (<p>         Selected</p>);
+      }
+    }
     return (
       <div>
         <p>{problem.id}</p>
+        { renderFocusView() }
       </div>
     )
   }
--- app/components/ManagerProblems.jsx
+++ app/components/ManagerProblems.jsx
@@ -6,9 +6,10 @@ import ManagerProblemView from 'ManagerProblemView';
 export var ManagerProblems = React.createClass({
   render: function(){
     var {problems} = this.props;
+    var selectedProblemView = 2;
     var renderProblemViews = () => {
       return problems.map((problem) => {
-        return <ManagerProblemView key={problem.id} problem={problem} />
+        return <ManagerProblemView key={problem.id} problem={problem} selectedProblemView={selectedProblemView}/>
       });
     }
     return (
